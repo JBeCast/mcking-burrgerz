f524cef7e24c57dbf113a89d380a7a6f7f62e854
JBeCast <jbcsound@gmail.com>
2017-12-30 22:04:39 +0100

    Created empty BurgerBuilder component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+
 import Aux from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const layout = props => (
   <Aux>
     <div>ToolBar, SideDrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
       {props.children}
     </main>
   </Aux>
