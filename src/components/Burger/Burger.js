737e15994ea54398f0345dd9866d78d2c61ff433
JBeCast <jbcsound@gmail.com>
2017-12-31 13:04:57 +0100

    Tested Burger with hardcoded ingredients in BurgerBuilder state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,11 +5,15 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = props => {
+  const ingredientsArr = Object.keys(props.ingredients).map(ingKey => {
+    return [...Array(props.ingredients[ingKey])].map((_, i) => (
+      <BurgerIngredient key={ingKey + i} type={ingKey} />
+    ));
+  })
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {ingredientsArr}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
