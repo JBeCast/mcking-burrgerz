8835055dd9380c7a68baaf3569036aaea6a9a6bb
JBeCast <jbcsound@gmail.com>
2018-01-14 23:40:28 +0100

    Passed order details fetched from api to each Order component.

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,29 @@ import React from 'react';
 import classes from './Order.css';
 
 const order = props => {
+  const ingredients = [];
+  for (let ingName in props.ingredients) {
+    ingredients.push({
+      name: ingName,
+      amount: props.ingredients[ingName]
+    });
+  }
+
+  const ingElements = ingredients.map(ing => <span
+    style={{
+      textTransform: 'capitalize',
+      display: 'inline-block',
+      margin: '0 8px',
+      border: '1px solid #ccc',
+      padding: '5px'
+    }}
+    key={ing.name}>{ing.name} ({ing.amount})
+  </span>);
+  
   return (
     <div className={classes.Order}>
-      <p>Ingredients: salad (1)</p>
-      <p>Price: <strong>USD 5.45</strong></p>
+      <p>Ingredients: {ingElements}</p>
+      <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
     </div>
   );
 };
