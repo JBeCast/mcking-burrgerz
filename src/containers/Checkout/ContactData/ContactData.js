b7044c096330c9950140eb2097ab0dc006015809
joaquin.bermudez <joaquin.bermudez@elementsinteractive.es>
2018-01-17 17:15:31 +0100

    Created form dynamically

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -49,7 +49,7 @@ class ContactData extends Component {
         value: ''
       },
       deliveryMethod: {
-        elementType: 'input',
+        elementType: 'select',
         elementConfig: {
           options: [
             {value: 'fastest', displayValue: 'Fastest'},
@@ -92,12 +92,23 @@ class ContactData extends Component {
   }
 
   render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      })
+    }
     let form = (
       <form action="">
-          <Input elementType="..." elementConfig="..." value="..." />
-          <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
-          <Input inputtype="input" type="text" name="street" placeholder="Street" />
-          <Input inputtype="input" type="text" name="postcode" placeholder="Postal code" />
+          {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+            />
+          ))}
           <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
     );
