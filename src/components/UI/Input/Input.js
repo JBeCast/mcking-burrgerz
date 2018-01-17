a6a27b1b4a8b313affe6b34b7dbc063db5f80664
joaquin.bermudez <joaquin.bermudez@elementsinteractive.es>
2018-01-17 17:14:07 +0100

    Created form dynamically

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -11,6 +11,21 @@ const input = props => {
     case ('input'):
       inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
       break;
+    case ('select'):
+      inputElement = (
+      <select
+        className={classes.InputElement}
+        value={props.value}>
+        {props.elementConfig.options.map(option => (
+          <option
+            key={option.value}
+            value={option.value}>
+              {option.displayValue}
+          </option>
+        ))}
+      </select>
+    )
+      break;
     default:
       inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />
       break;
