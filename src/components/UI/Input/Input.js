c40138d684509535f2bfb47dfdf96ba4040e853e
joaquin.bermudez <joaquin.bermudez@elementsinteractive.es>
2018-01-18 11:58:44 +0100

    Improved form validation by not marking fields as invalid when they haven't been touched yet

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,17 +4,23 @@ import classes from './Input.css';
 
 const input = props => {
   let inputElement = null;
+  const inputClasses = [classes.InputElement];
+
+  if (props.invalid && props.shouldValidate && props.touched) {
+    inputClasses.push(classes.Invalid);
+  }
+
   switch (props.elementType) {
     case ('textarea'):
       inputElement = <textarea
-        className={classes.InputElement} {...props.elementConfig}
+        className={inputClasses.join(' ')} {...props.elementConfig}
         value={props.value}
         onChange={props.changed}
       ></textarea>
       break;
     case ('input'):
       inputElement = <input
-        className={classes.InputElement} {...props.elementConfig}
+        className={inputClasses.join(' ')} {...props.elementConfig}
         value={props.value}
         onChange={props.changed}
       />
@@ -22,7 +28,7 @@ const input = props => {
     case ('select'):
       inputElement = (
       <select
-        className={classes.InputElement}
+        className={inputClasses.join(' ')}
         value={props.value}
         onChange={props.changed}
       >
@@ -38,7 +44,7 @@ const input = props => {
       break;
     default:
       inputElement = <input
-        className={classes.InputElement} {...props.elementConfig}
+        className={inputClasses.join(' ')} {...props.elementConfig}
         value={props.value}
         onChange={props.changed}
       />
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -18,7 +18,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       email: {
         elementType: 'input',
@@ -30,7 +31,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       street: {
         elementType: 'input',
@@ -42,7 +44,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       zipCode: {
         elementType: 'input',
@@ -56,7 +59,8 @@ class ContactData extends Component {
           minLength: 5,
           maxLength: 5
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       country: {
         elementType: 'input',
@@ -68,7 +72,8 @@ class ContactData extends Component {
         validation: {
           required: true
         },
-        valid: false
+        valid: false,
+        touched: false
       },
       deliveryMethod: {
         elementType: 'select',
@@ -137,6 +142,7 @@ class ContactData extends Component {
     }
     updatedFormElement.value = e.target.value;
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     this.setState({orderForm: updatedOrderForm});
   }
@@ -157,6 +163,9 @@ class ContactData extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              shouldValidate={formElement.config.validation}
+              invalid={!formElement.config.valid}
+              touched={formElement.config.touched}
               changed={e => this.inputChangedHandler(e, formElement.id)}
             />
           ))}
