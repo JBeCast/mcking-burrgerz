20c5067f25e720178226b021f281c1e674dec0d2
JBeCast <jbcsound@gmail.com>
2018-01-20 13:12:29 +0100

    Added missing style.

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -23,7 +23,7 @@ const buildControls = props => {
         />
       ))}
       <button
-        className={classes.orderButton}
+        className={classes.OrderButton}
         disabled={!props.purchasable}
         onClick={props.ordered}>ORDER NOW</button>
     </div>
