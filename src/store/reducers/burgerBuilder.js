9b0a90393abda5ccd32e81cef727a3819c6fafe2
JBeCast <jbcsound@gmail.com>
2018-01-28 15:44:07 +0100

    More reducers refactoring.

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -14,38 +14,49 @@ const INGREDIENT_PRICES = {
   bacon: .7
 }
 
+const addIngredient = (state, action) => {
+  const updatedIngredient = { [action.ingredient]: state.ingredients[action.ingredient] + 1 };
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  return updateObject(state, {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredient]
+  });
+}
+
+const removeIngredient = (state, action) => {
+  const updatedIngredientRm = {[action.ingredient]: state.ingredients[action.ingredient] - 1};
+  const updatedIngredientsRm = updateObject(state.ingredients, updatedIngredientRm);
+  return updateObject(state, {
+    ingredients: updatedIngredientsRm,
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredient]
+  });
+}
+
+const setIngredients = (state, action) => {
+  return updateObject(state, {
+    ingredients: {
+      // If the order's not specified, they are sorted alphabetically by Firebase
+      salad: action.ingredients.salad,
+      bacon: action.ingredients.bacon,
+      cheese: action.ingredients.cheese,
+      meat: action.ingredients.meat
+    },
+    error: false,
+    totalPrice: 3
+  });
+}
+
+const fetchIngredientsFailed = (state, action) => {
+  return updateObject(state, { error: true });  
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.ADD_INGREDIENT:
-      const updatedIngredient = {[action.ingredient]: state.ingredients[action.ingredient] + 1};
-      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-      return updateObject(state, {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredient]
-      });
-    case actionTypes.REMOVE_INGREDIENT:
-      const updatedIngredientRm = {[action.ingredient]: state.ingredients[action.ingredient] - 1};
-      const updatedIngredientsRm = updateObject(state.ingredients, updatedIngredientRm);
-      return updateObject(state, {
-        ingredients: updatedIngredientsRm,
-        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredient]
-      });
-    case actionTypes.SET_INGREDIENTS:
-      return updateObject(state, {
-        ingredients: {
-          // If the order's not specified, they are sorted alphabetically by Firebase
-          salad: action.ingredients.salad,
-          bacon: action.ingredients.bacon,
-          cheese: action.ingredients.cheese,
-          meat: action.ingredients.meat
-        },
-        error: false,
-        totalPrice: 3
-      });
-    case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return updateObject(state, { error: true });
-    default:
-      return state;
+    case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+    case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+    case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+    case actionTypes.FETCH_INGREDIENTS_FAILED: fetchIngredientsFailed(state, action);
+    default: return state;
   }
 }
 
